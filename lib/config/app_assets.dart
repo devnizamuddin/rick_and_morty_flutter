a28438377c045f62e5e7f498041af29400c4d637
devnizamuddin <dev.nizamuddin@gmail.com>
2024-04-03 12:27:43 +0600

    assets added for splash screen

---
+++ lib/config/app_assets.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: constant_identifier_names
+
+class AppAssets {
+  static const RICK_AND_MORTY_CIRCLE =
+      'assets/images/rick_and_morty_circle.png';
+  static const RICK_AND_MORTY_TEXT = 'assets/images/rick_and_morty_text.png';
+  static const SPLASH_FRAME = 'assets/images/splash_frame.png';
+}
