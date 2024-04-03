958156e08c381cc5a6ae7d55d0da7d3ea664382d
devnizamuddin <dev.nizamuddin@gmail.com>
2024-04-03 23:46:41 +0600

    run on ios and theme fixed

--- lib/config/app_theme.dart
+++ lib/config/app_theme.dart
@@ -4,13 +4,18 @@ class AppTheme {
   static final ThemeData _themeData = ThemeData(
     useMaterial3: true,
     colorScheme: _colorScheme,
-    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-      backgroundColor: Color(0xff191D29),
-      unselectedItemColor: Colors.white,
+    bottomNavigationBarTheme: _bottomNavigationBarTheme,
+    appBarTheme: const AppBarTheme(
+      iconTheme: IconThemeData(color: Colors.white),
     ),
   );
   ThemeData get themeData => _themeData;
 
+  static const BottomNavigationBarThemeData _bottomNavigationBarTheme =
+      BottomNavigationBarThemeData(
+    backgroundColor: Color(0xff191D29),
+    unselectedItemColor: Colors.white,
+  );
   static const ColorScheme _colorScheme = ColorScheme.light(
     primary: Color(0xff9DFE00),
     secondary: Color(0xff13D9E5),
